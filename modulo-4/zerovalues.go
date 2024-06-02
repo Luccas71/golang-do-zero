bd45925d123e39565f54d298e922d5c308716e7c
Lucas Volpato <lmaciel916@gmail.com>
2024-06-01 21:20:14 -0300

    zerovalues

---
+++ modulo-4/zerovalues.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//valores padrão atribuidos quando a variavel é declarada
+	var i int
+	var f float64
+	var b bool
+	var s string
+
+	fmt.Printf("Inteiro: %v\n", i)
+	fmt.Printf("Float: %v\n", f)
+	fmt.Printf("Boolean: %v\n", b)
+	fmt.Printf("String: %v\n", s)
+}
